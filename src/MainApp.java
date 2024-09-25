59b4b389fdd9ac4cadfff63da3f5b29ae6fcc6d4
Ericharw <ericharizki12@gmail.com>
2024-09-25 12:50:59 +0700

    feat: perubahan kode Pembayaran

--- src/MainApp.java
+++ src/MainApp.java
@@ -10,19 +10,21 @@ public class MainApp {
         Konten konten1 = new Konten(1, "Artikel", 15, "Belajar dasar permograman Java");
         konten1.tampilkan();
 
-        Pembayaran pembayaran1 = new Pembayaran(1, "2024-09-24", 500000.00, 101, 202);
+        Pembayaran pembayaran1 = new Pembayaran(1, "2024-09-25", 500000.0, 101, 202);
+        Pembayaran pembayaran2 = new Pembayaran(2, "2024-09-26", 750000.0, 102, 203);
 
+        System.out.println("Pembayaran 1");
         System.out.println("ID Pembayaran: " + pembayaran1.getId());
-        System.out.println("Tanggal Pembayaran: " + pembayaran1.getTanggal());
-        System.out.println("Jumlah Pembayaran: " + pembayaran1.getJumlah());
+        System.out.println("Tanggal: " + pembayaran1.getTanggal());
+        System.out.println("Jumlah: " + pembayaran1.getJumlah());
         System.out.println("ID Peserta: " + pembayaran1.getPesertaId());
         System.out.println("ID Kursus: " + pembayaran1.getKursusId());
 
-        pembayaran1.setJumlah(550000.00);
-        pembayaran1.setTanggal("2024-09-25");
-
-        System.out.println("\nSetelah perubahan:");
-        System.out.println("Tanggal Pembayaran: " + pembayaran1.getTanggal());
-        System.out.println("Jumlah Pembayaran: " + pembayaran1.getJumlah());
+        System.out.println("\nPembayaran 2");
+        System.out.println("ID Pembayaran: " + pembayaran2.getId());
+        System.out.println("Tanggal: " + pembayaran2.getTanggal());
+        System.out.println("Jumlah: " + pembayaran2.getJumlah());
+        System.out.println("ID Peserta: " + pembayaran2.getPesertaId());
+        System.out.println("ID Kursus: " + pembayaran2.getKursusId());
     }
 }
